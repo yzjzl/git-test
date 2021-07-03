a180002ad9f0089ac8988986426ed3b1e0c148e5
yzj <2714476123@qq.com>
2021-07-03 15:39:29 +0800

    push commit1

--- src/main/java/com/yzj/Git_demo.java
+++ src/main/java/com/yzj/Git_demo.java
@@ -7,6 +7,7 @@ public class Git_demo {
         System.out.println("hello git3");
         System.out.println("master commit");
         System.out.println("hot-fix commit");
+        System.out.println("push test");
     }
 
 }
