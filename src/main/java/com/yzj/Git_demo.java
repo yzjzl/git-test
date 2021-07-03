9308befe3585db894ab481fc565e57f7c2b54a67
yzj <2714476123@qq.com>
2021-07-03 14:35:54 +0800

    second commit

--- src/main/java/com/yzj/Git_demo.java
+++ src/main/java/com/yzj/Git_demo.java
@@ -3,6 +3,7 @@ package com.yzj;
 public class Git_demo {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 
 }
