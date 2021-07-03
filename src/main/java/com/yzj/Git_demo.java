6aeb1e70eaa1e44940c14ac31077c61f4dd18cc0
yzj <2714476123@qq.com>
2021-07-03 14:36:29 +0800

    third commit

--- src/main/java/com/yzj/Git_demo.java
+++ src/main/java/com/yzj/Git_demo.java
@@ -4,6 +4,7 @@ public class Git_demo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 
 }
